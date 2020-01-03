1befb9e675ff5c4c77f5e0ed802b328e4faa4ea9
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2020-01-02 22:34:35 -0300

    Criação de CRUD com alunoDAO em Lista

--- Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,9 +2,15 @@ package com.example.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.agenda.DAO.AlunoDAO;
 import com.example.agenda.R;
+import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +18,26 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final AlunoDAO dao = new AlunoDAO();
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        View botaoSalvar =  findViewById(R.id.activity_formulario_aluno_botao_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+               Aluno alunoCriado =  new Aluno(nome,telefone, email);
+               dao.salva(alunoCriado);
+               startActivity(new Intent(FormularioAlunoActivity.this, ListaAlunosActivity.class));
+            }
+        });
     }
+
+
 }
